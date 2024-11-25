df59a76676eebcb4346444c8bd4d5062821c0594
Lê Khoa <lekhoa@MacBook-Pro-cua-Le.local>
2024-11-25 08:42:22 +0700

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-500 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-lg font-bold">My Website</h1>
+        <ul className="flex space-x-4">
+          <li>
+            <Link to="/" className="hover:underline">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:underline">
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to Home Page!</h1>
+    </div>
+  );
+};
+
+export default Home;
